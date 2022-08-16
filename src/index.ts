16c478d1f237c8acea563a4deb1f15b281258172
malashchuk~ <malashchukk@gmail.com>
2022-08-16 16:18:54 +0300

    feat: add routing (Tue, Aug 16, 16:18:40)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 import "./style.scss";
+import Router from "./components/controller/Router";
+
+const router = new Router({
+  mode: "hash",
+  root: "/",
+});
+
+// example of routing work
+router
+  .add(/home/, () => {
+    alert("welcome in home page");
+  })
+  .add(/users\/(.*)/, (id: number) => {
+    alert(`userId: ${id}`);
+  });
