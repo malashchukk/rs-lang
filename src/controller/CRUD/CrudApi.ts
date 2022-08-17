f40d3d1147a9c3473e35f22fb246a26f3a1e85e5
Nik-portfolio <kravtcovmikita@gmail.com>
2022-08-17 19:30:16 +0300

    feat: add CRUD methods

---
+++ src/controller/CRUD/CrudApi.ts
@@ -0,0 +1,62 @@
+import Loader from "./loader";
+import { urlOptions, response } from "./loader";
+class CrudApi extends Loader {
+  constructor(baseLink: string, options: urlOptions) {
+    super(baseLink, options);
+  }
+  async createItem<T>(
+    { endpoint, options }: { endpoint: string; options?: object },
+    body: T,
+    token = "",
+    fetchParam = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  ): Promise<response<T>> {
+    return this.load(
+      { ...fetchParam, body: JSON.stringify(body) },
+      endpoint,
+      options
+    );
+  }
+
+  async deleteItem<T>(
+    { endpoint, options }: { endpoint: string; options?: object },
+    token = "",
+    fetchParam = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+      },
+    }
+  ): Promise<response<T>> {
+    return this.load(fetchParam, endpoint, options);
+  }
+
+  async updateItems<T>(
+    { endpoint, options }: { endpoint: string; options?: object },
+    body: T,
+    token = "",
+    fetchParam = {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  ): Promise<response<T>> {
+    return this.load(
+      { ...fetchParam, body: JSON.stringify(body) },
+      endpoint,
+      options
+    );
+  }
+}
+const crudApi = new CrudApi("https://rslang-malashchukk.herokuapp.com", {});
+export default crudApi;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 import "./style.scss";
+import "./controller/CRUD/CrudApi";
