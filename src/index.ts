d9b1fca29772057981d97adee0dff13664b4e133
DelfiaHope <valeruanovna94@mail.ru>
2022-08-20 17:21:13 +0300

    add: section About Team

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import "./components/controller/CRUD/CrudApi";
 import router from "./components/controller/Router";
 import { Registration } from "./components/registration/registrationForm";
 import { Main } from "./components/main/main";
+import { About } from "./components/aboutPage/aboutPage"
 
-
+const aboutTeam = new About;
+aboutTeam.ListenerBtnAbout()
 const mainText = new Main;
 mainText.showMain()
 mainText.ListenerLogo()
