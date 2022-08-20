e999599afbd268b330ab2a9e5749004f02ef81ca
DelfiaHope <valeruanovna94@mail.ru>
2022-08-20 16:28:17 +0300

    fix: main in separate file, add registration form

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import "./style/header.scss";
 import "./style/footer.scss";
-import "./style/main.scss";
 import "./components/controller/CRUD/CrudApi";
 import router from "./components/controller/Router";
+import { Registration } from "./components/registration/registrationForm";
+import { Main } from "./components/main/main";
+
+
+const mainText = new Main;
+mainText.showMain()
+mainText.ListenerLogo()
+const register = new Registration;
+register.ListenerBtn()
 
 router.add(/home/, () => {
-  alert("welcome in home page");
+  //alert("welcome in home page");
 });
 // router.add(/user/, () => {
 //   alert(`userId`);
@@ -13,4 +21,6 @@ router.add(/home/, () => {
 // router.add(/user\/nikita/, () => {
 //   alert(`userId nikita`);
 // });
+
+
 
