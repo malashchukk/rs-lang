65a6d0e4719241910627e6314239c845605c01b2
malashchuk~ <malashchukk@gmail.com>
2022-08-22 17:38:37 +0300

    refactor: clean index.ts (Mon, Aug 22, 17:38:35)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import "./style/style.scss";
-import "./components/controller/CRUD/CrudApi";
-import router from "./components/controller/Router";
 import { Registration } from "./components/registration/registrationForm";
 import { Main } from "./components/main/main";
 import { About } from "./components/aboutPage/aboutPage";
@@ -12,7 +9,3 @@ mainText.showMain();
 mainText.ListenerLogo();
 const register = new Registration();
 register.ListenerBtn();
-
-router.add(/home/, () => {
-  //alert("welcome in home page");
-});
