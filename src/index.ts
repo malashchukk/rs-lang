2d9305c1c6c4518c76ebf18eab6687d645d0660a
malashchuk~ <malashchukk@gmail.com>
2022-08-29 16:58:02 +0300

    fix: edit index.ts (Mon, Aug 29, 16:58:05)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import "./style/style.scss";
 import "./components/controller/CRUD/CrudApi";
-import router from "./components/controller/Router";
-import { Registration } from "./components/registration/registrationForm";
-import { Main } from "./components/main/main";
-import { About } from "./components/aboutPage/aboutPage"
+import { Main } from "./components/view/main/main";
+import { About } from "./components/view/aboutPage/aboutPage";
 import user from "./components/controller/authorization/authorization";
 import popUp from "./components/view/popUpAuthorization/popUpAuthorization";
 
-const aboutTeam = new About;
-aboutTeam.ListenerBtnAbout()
-const mainText = new Main;
-mainText.showMain()
-mainText.ListenerLogo()
-const register = new Registration;
-register.ListenerBtn()
+const aboutTeam = new About();
+aboutTeam.ListenerBtnAbout();
+const mainText = new Main();
+mainText.showMain();
+mainText.ListenerLogo();
 
 popUp.replacePopUpButton("unauthorized");
 user.isAuthorization();
